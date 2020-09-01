f6980a512279b2fdfc05efb05050eeef69f7a359
Xsoda <Xsoda@live.com>
2020-09-01 21:15:45 +0800

    rewrite

---
+++ CMakeFiles/cbor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cbor.dir/cbor.c.o"
+  "CMakeFiles/cbor.dir/json.c.o"
+  "libcbor.a"
+  "libcbor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cbor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
